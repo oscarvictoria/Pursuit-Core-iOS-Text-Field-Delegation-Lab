393361d6ea909baa657b5f2d35c1117e3a392715
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-11 18:34:14 -0500

    updated

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: View Controller Properties
+    
+    @IBOutlet weak var scrambledWord: UILabel!
+    
+    
+    @IBOutlet weak var userInput: UITextField!
+    
+    
+    
+    
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: Actions and Methods
+    
+    
+    @IBAction func userInputAction(_ sender: UITextField) {
+    }
+    
+    
+    
+    @IBAction func nextWord(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
