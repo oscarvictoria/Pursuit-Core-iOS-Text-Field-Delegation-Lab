59e411bd094b5c4abd00cf9cec18b85881b6d7d1
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-13 14:16:13 -0500

    Updated TextTwist ViewController

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -12,69 +12,50 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: View Controller Properties
     
-    @IBOutlet weak var scrambledWord: UILabel!
-    
-    
+    @IBOutlet weak var scrambledWordLabel: UILabel!
     @IBOutlet weak var userInput: UITextField!
-    
     @IBOutlet weak var nextWordOutlet: UIButton!
     
-    var randomScrambledWord = String()
     
-    var instanceOfWord = Word.getRandomWord()
-    
-    var randomWord = String()
+    //var scrambledWord = Word.getRandomWord()
+    var randomWord = Word.getRandomWord()
+    var scrambledWord = String()
     
  
-//    func getUnscrambledWord(_ someWord:String) -> String {
-//
-//        for word in instanceOfWord {
-//            let someWord = Word.getRandomWord().scrambled
-//            let unscrambledWord = Word.getRandomWord().unscrambled
-//        }
-//
-//        return unscrabledWord
-//    }
-//
+
+    //MARK: viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         userInput.delegate = self
+        
     }
     
     // MARK: Actions and Methods
     
-    
-    @IBAction func userInputAction(_ sender: UITextField) {
+    @IBAction func userInputWhileEditing(_ sender: UITextField) {
+        //instanceOfWord
+        print(sender.text ?? "ERROR")
+        scrambledWord = randomWord.scrambled
+        if scrambledWord.contains(String(sender.text ?? "ERROR")) {
+            print("We have a match")
+        }
     }
-    
-    
     
     @IBAction func nextWord(_ sender: UIButton) {
-        randomScrambledWord =
-        Word.getRandomWord().scrambled
-        scrambledWord.text = randomScrambledWord
-       // print(randomScrambledWord)
+        print(randomWord)
+        print(scrambledWord)
+        
+        scrambledWordLabel.text = String(scrambledWord)
+        nextWordOutlet.isEnabled = false
     }
     
-    
-    
-    
-    
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-       
         userInput.resignFirstResponder()
         return true
     }
-    
-    
-    
-    
-    
-//    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-//        print("TextFieldShould clear")
-//        return true
-//    }
-    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
 }
 
